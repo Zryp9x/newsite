f55c428cd089ef03a396a5e484245d1aa1e4f196
Zryp9x <sneefeeh@gmail.com>
2021-04-28 22:28:32 +0200

    general update

--- index.php
+++ index.php
@@ -1,14 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
-</head>
-<body>
-
-<?php echo 'hehe' ?>
+   
+   <?php 
+        $title = "Index";
+        include 'includes/header.php' 
+    ?>
+    <!-- Basic HTML -->
+    <h1>Hello HTML - PHP Primer</h1>
+    <br/>
+    <?php  
+        //Printing to HTML using echo
+        echo 'Hello PHP!';
+        // YOu can echo HTML tags
+        echo '<br/>';
+        echo 'Second Line';
+        echo '<br/>';
     
-</body>
-</html>
+        // Variables need a '$'. They are not strongly typed
+        $name = 'Trevoir Williams';
+        $age = 31;
+        // echo variable
+        echo $name;
+        echo '<h1>My Name Is: '.$name.' </h1>';
+        echo '<h1>My Age Is: '.$age.' </h1>';
+        // Echo using double quotes and interpolation
+        echo "<h1>My Name is: $name </h1>";
+    ?>
+    <button type="button" class="btn btn-dark">CLICK ME!</button>
+    <button type="button" class="btn btn-primary">CLICK ME!</button>
+    <button type="button" class="btn btn-success">CLICK ME!</button>
+    <a href="https://www.heroku.com" target="_blank" class="btn btn-danger">Heroku.com</a>
+    <?php
+        require 'includes/footer.php' 
+    ?>
